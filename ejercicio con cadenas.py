b392aac2d30c3cdd23ef75280800ec9905724682
Xavi Serrano <xserrano2007@gmail.com>
2026-09-07 23:02:41 -0600

    Delete ejercicio con cadenas.py

--- ejercicio con cadenas.py
+++
@@ -1,7 +0,0 @@
-contraseña=input("Ingrese su contraseña: ")  
-contraseña_separada=contraseña.split(",")
-print(contraseña_separada)
-contraseña_minusculas=contraseña.lower()
-print(contraseña_minusculas)
-contraseña_subcadena=contraseña[0:5]
-print(contraseña_subcadena)
