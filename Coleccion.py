2ea23fa1b269328439a463048c90c3aa72803042
Xavi Serrano <xserrano2007@gmail.com>
2026-09-15 00:48:44 -0600

    Add files via upload

---
+++ Coleccion.py
@@ -0,0 +1,20 @@
+inventario = {
+    "manzana": 10,
+    "banano": 5,
+    "uva": 8,
+    "mango": 7
+    }
+         
+print(inventario)
+
+inventario["fresa"] = 25
+
+print(inventario)
+
+inventario["manzana"] = 0
+
+print(inventario)
+
+del inventario["uva"]
+
+print(inventario)
