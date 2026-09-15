90a3deba89ca056aa3818ed30abdb446f85a639f
Xavi Serrano <xserrano2007@gmail.com>
2026-09-14 18:24:58 -0600

    Add files via upload

---
+++ Listas.py
@@ -0,0 +1,29 @@
+semana = ["Lunes","martes","miercoles","jueves", "viernes","sabado","domingo"]
+
+print("Dias de la semana:\n")
+print(semana)
+
+año = ["Enero", "Febrero", "Marzo","Abril","Mayo","Junio",
+         "Julio","Agosto","septiembre","octubre","noviembre", "diciembre"]
+
+print("Meses del año:\n")
+print(año)
+
+semana.remove("Lunes")
+print(semana)
+
+
+semana.append("Lunes")
+print(semana)
+
+año.sort()
+print(año)
+
+
+año.sort(reverse=True)
+print(año)
+
+año = ["Enero", "Febrero", "Marzo","Abril","Mayo","Junio",
+         "Julio","Agosto","septiembre","octubre","noviembre", "diciembre"]
+
+print(año)
